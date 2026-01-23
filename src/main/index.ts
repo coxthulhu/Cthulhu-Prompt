213b546c3e45dcd1b292cf4e4313ec5e89eed881
coxthulhu <252392645+coxthulhu@users.noreply.github.com>
2026-01-23 00:17:37 +0000

    Split userData per environment

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,6 +1,24 @@
+import { app } from 'electron'
+import { join } from 'path'
 import { startupNormally } from './NormalStartup'
 import { setupTestStartupListener } from './TestStartup'
-import { isPlaywrightEnvironment } from './appEnvironment'
+import { isDevEnvironment, isPlaywrightEnvironment } from './appEnvironment'
+
+const DEV_USER_DATA_DIRECTORY_NAME = 'cthulhupromptdev'
+const PLAYWRIGHT_USER_DATA_DIRECTORY_NAME = 'cthulhupromptplaywright'
+
+const configureUserDataPath = (): void => {
+  if (isPlaywrightEnvironment()) {
+    app.setPath('userData', join(app.getPath('appData'), PLAYWRIGHT_USER_DATA_DIRECTORY_NAME))
+    return
+  }
+
+  if (isDevEnvironment()) {
+    app.setPath('userData', join(app.getPath('appData'), DEV_USER_DATA_DIRECTORY_NAME))
+  }
+}
+
+configureUserDataPath()
 
 const shouldUsePlaywrightSetup = isPlaywrightEnvironment()
 
